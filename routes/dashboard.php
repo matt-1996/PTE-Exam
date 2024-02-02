13df26a707289e25761e09921a36f968b7ffb2f1
Matt <matinnasirlahijani@gmail.com>
2024-02-02 22:39:09 +0330

    caching added into other routes

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,6 +3,11 @@ use App\Http\Controllers\FaqController;
 use App\Http\Controllers\Order\SubscriptionPlanController;
 use App\Http\Controllers\Profile\CourseController;
 use App\Http\Controllers\Profile\DashboardController;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
+
+Route::group(['middleware' => ['auth' , CacheResponse::class]], function(){
+
+
 
 Route::get("home", [DashboardController::class , 'index'])->name("dashboard.home");
 
@@ -15,3 +20,5 @@ Route::get('faq', [FaqController::class , 'index'])->name('dashboard.faq');
 Route::get('contact-us', [DashboardController::class ,'contactUsView'])->name('dashboard.contact-us');
 
 Route::get('settings', [DashboardController::class ,'settingsView'])->name('dashboard.settings.view');
+
+});
